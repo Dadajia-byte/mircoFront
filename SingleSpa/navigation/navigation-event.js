8f72d7972bb7056c44d2201451d7a5d8ea2411a4
Dadajia-byte <1814650097@qq.com>
2025-05-30 16:29:50 +0800

    feat: single-spa简易原理实现

--- SingleSpa/navigation/navigation-event.js
+++ SingleSpa/navigation/navigation-event.js
@@ -3,9 +3,59 @@
 import { reroute } from "./reroute.js";
 
 function urlRoute() {
-  reroute();
+  reroute(arguments);
 }
 
 window.addEventListener('hashchange', urlRoute);
 
 window.addEventListener('popstate', urlRoute); // 浏览器历史切换
+
+// 但是路由切换时，我们触发single-spa的addEventListener，应用当中可能也会存在addEventListener
+// 所以应用中对路由的监听，我们必须让他在新应用挂载完毕后，再进行监听；因此需要劫持原生的路由系统
+
+const captureEventListener = {
+  hashchange: [],
+  popstate: [],
+};
+const listeningTo = ['hashchange', 'popstate'];
+const originalAddEventListener = window.addEventListener;
+const originalRemoveEventListener = window.removeEventListener;
+window.addEventListener = function(eventName, cb) {
+  // 有要监听的事件，函数不能重复
+  if (listeningTo.includes(eventName) && captureEventListener[eventName].some(fn => fn === cb)) {
+    return captureEventListener[eventName].push(cb);
+  }
+  return originalAddEventListener.apply(this, arguments);
+}
+window.removeEventListener = function(eventName, cb) {
+  // 有要监听的事件，函数不能重复
+  if (listeningTo.includes(eventName)) {
+    captureEventListener[eventName] = captureEventListener[eventName].filter(fn => fn !== cb);
+    return;
+  }
+  return originalRemoveEventListener.apply(this, arguments);
+}
+
+export function callCaptureEventListener(e) {
+  if (e) { // 如果有事件对象，说明是路由切换
+    const eventName = e[0].type;
+    if (listeningTo.includes(eventName)) {
+      captureEventListener[eventName].forEach(fn => fn.apply(this, e));
+    }
+  }
+}
+
+function patchFn(updateState, methodName) {
+  return function() {
+    const urlBefore = window.location.href;
+    const r = updateState.apply(this, arguments); // 调用此方法确实发生了路由切换
+    const urlAfter = window.location.href;
+    if (urlBefore !== urlAfter) {
+      window.dispatchEvent(new PopStateEvent("popstate")); // 手动触发popstate事件
+    }
+    return r;
+  }
+}
+
+window.history.pushState = patchFn(window.history.pushState, 'pushState');
+window.history.replaceState = patchFn(window.history.replaceState, 'replaceState');
--- SingleSpa/navigation/reroute.js
+++ SingleSpa/navigation/reroute.js
@@ -4,16 +4,30 @@ import { toLoadPromise } from "../lifecycles/load.js";
 import { toMountPromise } from "../lifecycles/mount.js";
 import { toUnmountPromise } from "../lifecycles/unmount.js";
 import { started } from "../start.js";
-import './navigation-event.js'
+import { callCaptureEventListener } from "./navigation-event.js";
 
-export const reroute = () => {
+let appChangeUnderWay = false;
+let peopleWaitingOnAppChange = [];
+
+export const reroute = (event) => {
   const { appsToLoad, appsToUnmount, appsToMount } = getAppChanges();
-  
+
+  // 如果多次触发reroute方法，我们可以创建一个队列来屏蔽这个问题
+  if (appChangeUnderWay) {
+    return new Promise((resolve, reject)=>{
+      peopleWaitingOnAppChange.push({
+        resolve,
+        reject,
+      });
+    })
+  }
+
   // 加载完毕后 需要去挂载应用
 
   // 先拿到应用加载 -》 
   if (started) {
     // 用户调用过start方法 我们需要处理当前应用挂载或者卸载
+    appChangeUnderWay = true;
     return performAppChange()
   }
 
@@ -21,23 +35,42 @@ export const reroute = () => {
   return loadApps();
 
   function loadApps() {
-    return Promise.all(appsToLoad.map(toLoadPromise));
+    return Promise.all(appsToLoad.map(toLoadPromise)).then(callEventListeners);
   }
   function performAppChange() {
     // 将不需要的应用卸载掉
-    // 加载需要的应用 -》 启动对应的应用 -》 挂载对应的应用
+    // 加载需要的应用 -》 启动对应的应用 -》卸载之前的应用 -》 挂载对应的应用
     // 1. 卸载应用
     const unmountPromise = Promise.all(appsToUnmount.map(toUnmountPromise));
     // 2. 加载应用(可能这个应用在注册的时候就已经被加载了)
-    const mountPromise = Promise.all(appsToLoad.map(app=>toLoadPromise(app).then(app=>{
+    const loadMountPromise = Promise.all(appsToLoad.map(app=>toLoadPromise(app).then(app=>{
       // 当应用加载完毕后，需要启动和挂载，但是要保证挂载前，先卸载应用
       return tryBootstrapAndMount(app, unmountPromise);
     })));
+    // 如果应用没有加载过 加载-》启动-》挂载； 如果应用加载过了 挂载
+    const mountPromise = Promise.all(appsToMount.map(app=>tryBootstrapAndMount(app, unmountPromise)));
+
     function tryBootstrapAndMount(app, unmountPromise) {
       if(shouldBeActive(app)) {
         // 保证卸载完毕再挂载
         return toBootstrapPromise(app).then(app=>unmountPromise.then(()=>toMountPromise(app)))
       }
     }
+    
+    return Promise.all([loadMountPromise, mountPromise]).then(()=>{
+      callEventListeners();
+      appChangeUnderWay = false;
+      if (peopleWaitingOnAppChange.length > 0) {
+        // 如果有等待的队列，说明之前有reroute方法被调用
+        peopleWaitingOnAppChange.forEach(({ resolve }) => resolve());
+        peopleWaitingOnAppChange = [];
+      }
+    });
   }
+
+  function callEventListeners() {
+    callCaptureEventListener(event)
+  }
+
+
 }
